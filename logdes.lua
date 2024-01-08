0c456ad9d13bb2e3f3cce45a005e4d91476ef47a
Kawaei <95129136+Kawaei999@users.noreply.github.com>
2024-01-09 01:03:59 +0700

    Update logdes.lua

--- logdes.lua
+++ logdes.lua
@@ -10,8 +10,7 @@ _G.Teams = 'Pirates' -- เลือกทีม
 _G.MuteVolume = false -- true / false ปิดเสียงเกม
 _G.No_Nexus_Checker = true -- true / false ไม่เช็ค Nexus ว่ารันหรือยัง 
 _G.timer = 2.5 -- เวลาที่จะ set log หน่วยเป็นวินาที
-_G.Inventory = {"Hallow Scythe", "Tushita", "Shark Anchor", "Cursed Dual Katana", "True Triple Katana", "Dark Dagger", "Soul Guitar"} -- {"Yama", "Hallow Scythe", "Tushita", "Cursed Dual Katana", "True Triple Katana", "Dark Dagger", "Soul Guitar"} เลือกเอาวาจะ log ชิ้นไหนบ้าง
-
+_G.Inventory = {"Hallow Scythe", "Tushita", "Shark Anchor", "Cursed Dual Katana", "True Triple Katana", "Dark Dagger", "Soul Guitar"} -- {"Yama", "Hallow Scythe", "Tushita", "Cursed Dual Katana", "True Triple Katana", "Dark Dagger", "Soul Guitar"}
 _G.Key ="NKYQMI"
 _G.Discord = "887696519006941225"
 getgenv().Loaders = loadstring(game:HttpGet("https://raw.githubusercontent.com/Achitsak/scripts/main/masterp_"))()
