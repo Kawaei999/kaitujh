55b2b96d6f002cd946a860531bfedb8c63e3760f
Kawaei <95129136+Kawaei999@users.noreply.github.com>
2024-01-10 12:00:31 +0700

    Update logdes.lua

--- logdes.lua
+++ logdes.lua
@@ -1,4 +1,4 @@
-
+repeat task.wait() until game:IsLoaded() -- เปลี่ยนไปตาม Scripts ที่ใช้ฟามด้วย
 -- Settings Configs
 getgenv().Settings = {
     Debug = false, -- Debug Message ตอน set ข้อมูล
@@ -7,10 +7,11 @@ getgenv().Settings = {
 
 -- Main Configs
 _G.Teams = 'Pirates' -- เลือกทีม
-_G.MuteVolume = false -- true / false ปิดเสียงเกม
+_G.MuteVolume = true -- true / false ปิดเสียงเกม
 _G.No_Nexus_Checker = true -- true / false ไม่เช็ค Nexus ว่ารันหรือยัง 
 _G.timer = 2.5 -- เวลาที่จะ set log หน่วยเป็นวินาที
-_G.Inventory =  {"Yama", "Hallow Scythe", "Tushita", "Cursed Dual Katana", "True Triple Katana", "Dark Dagger", "Soul Guitar", "Shark Anchor"}
+_G._Inventory_ = {"Hallow Scythe", "Tushita", "Cursed Dual Katana", "Shark Anchor", "True Triple Katana", "Dark Dagger", "Soul Guitar"} -- {"Yama", "Hallow Scythe", "Tushita", "Cursed Dual Katana", "True Triple Katana", "Dark Dagger", "Soul Guitar"} เลือกเอาวาจะ log ชิ้นไหนบ้าง
+-- [[ - Script Here - ]]
 
 _G.Key ="NKYQMI"
 _G.Discord = "887696519006941225"
